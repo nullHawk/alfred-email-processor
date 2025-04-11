2c9304af8466895024d1621730aaf125ffc280c0
Suryansh <suryanshsinghshakya1@gmail.com>
2025-04-11 05:39:43 +0000

    add: state

---
+++ app/state.py
@@ -0,0 +1,13 @@
+import os 
+from typing import TypedDict, List, Dict, Any, Optional
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+class EmailState(TypedDict):
+    email: Dict[str, Any]
+    email_category: Optional[str]
+    spam_reason: Optional[str]
+    is_spam: Optional[bool]
+    email_draft: Optional[str]
+    messages: List[Dict[str, Any]]
